b7372cd527dfc29ca40621f393e5999216cb235c
ludohenin <ludovic.henin@yahoo.com>
2015-07-11 15:38:20 +0200

    Refactor SetActive directive

--- app/directives/set-active.ts
+++ app/directives/set-active.ts
@@ -1,71 +1,73 @@
-import {Directive, ElementRef, Inject} from 'angular2/angular2';
-import {Router} from 'angular2/router';
+import {
+  Directive,
+  ElementRef,
+  Inject,
+  Renderer,
+  QueryList,
+  Query
+} from 'angular2/angular2';
+import {Router, Location} from 'angular2/router';
 
 /**
  * Simple directive to add class active on a LI element when
- * its A child element is clicked. Active class is removed
- * from all other LI element.
+ * its A child element is clicked or on page load. Active class
+ * is removed from all other LI element.
  * Follow the same principle as nav's in Bootstrap.
  *
  * @Example:
- * 	<ul>
- * 		<li class="active" set-active>
+ * 	<ul set-active>
+ * 		<li>
  * 			<a href="a1">link 1</a>
  * 		</li>
- * 		<li set-active>
+ * 		<li>
  * 			<a href="a">link 2</a>
  * 		</li>
  * 	</ul>
  */
-@Directive({
-  selector: '[set-active]',
-  host: { '(^click)': 'setActive($event)' }
-})
-export class SetActive {
-  nativeElement: HTMLLIElement;
-  parent: HTMLUListElement;
-  mode: string
-  constructor(private element: ElementRef, @Inject(Router) private router) {
-    this.nativeElement = this.element.nativeElement;
-    if (this.nativeElement.tagName !== 'LI') {
-      throw new Error(`This directive only supports UL > LI list and must be
-                      applied on LI element`);
-    }
-    this.parent = <HTMLUListElement>this.nativeElement.parentNode;
-
-    // Reset as the view seems to be cached. Class active is still here
-    // even after a re-instanciation of the component using this directive.
-    this.removeActiveClass();
-
-    this.onLoadSetActive();
-  }
-  setActive(evt): void {
-    if (evt.target.tagName === 'A') {
-      this.removeActiveClass();
-      this.addActiveClass();
-    }
-  }
-  onLoadSetActive() {
-    // This is executed before the NgFor hence the timeout.
-    // TODO: see if there is a better way.
-    setTimeout(() => {
-      let route = this.router.parent._currentInstruction.accumulatedUrl;
-      let href = this.getHrefAttribute();
-      if (route.match(href)) { this.addActiveClass(); }
-    }, 0);
-  }
-  private removeActiveClass(): void {
-    let elements = this.parent.getElementsByClassName('active');
-    for (let i = 0; i < elements.length; i++) {
-      (<HTMLLIElement>elements[i]).classList.remove('active');
-    };
-  }
-  private addActiveClass(): void {
-    this.nativeElement.classList.add('active');
-  }
-  private getHrefAttribute() {
-    let anchorElement = <HTMLAnchorElement>this.nativeElement
-      .getElementsByTagName('A')[0];
-    return anchorElement.getAttribute('href');
+@Directive({ selector: 'a' })
+class Link {
+  constructor (private _elRef: ElementRef) {}
+  get href(): string {
+    return this._elRef.nativeElement.getAttribute('href');
   }
 }
+
+@Directive({ selector: 'li' })
+class ListItem {
+  className = 'active';
+  constructor(@Query(Link) public links: QueryList<Link>,
+              @Inject(Location) public location: Location,
+              private _elRef: ElementRef,
+              private _renderer: Renderer) {}
+  toggleClass() {
+    if (this.linkHref === this.location.path()) {
+      this._renderer.setElementClass(this._elRef, this.className, true);
+    } else {
+      this._renderer.setElementClass(this._elRef, this.className, false);
+    }
+  }
+  get linkHref(): string {
+    return this.links.first.href;
+  }
+}
+
+@Directive({ selector: '[set-active]', host: { '(^click)': 'setActive()' } })
+class List {
+  _href;
+  constructor(@Query(ListItem) private _items: QueryList<ListItem>,
+              @Inject(Location) private location: Location,
+              @Inject(Router) private _router: Router,
+              private _elRef: ElementRef) {
+    console.log(this._elRef);
+    console.log(this._items);
+    console.log(this.location.path());
+    // _items is populated later on it's refers to child classes.
+    // So we wait for changes.
+    this._items.onChange(() => this.setActive());
+  }
+  setActive(): void {
+    this._items._results.forEach(item => item.toggleClass())
+  }
+}
+
+export var SetActive = [Link, ListItem, List];
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,8 +1,8 @@
 
 /// <reference path="custom.system.d.ts" />
-/// <reference path="angular2/router.d.ts" />
 /// <reference path="custom.http.d.ts" />
 /// <reference path="angular2/angular2.d.ts" />
+/// <reference path="angular2/router.d.ts" />
 /// <reference path="es6-promise/es6-promise.d.ts" />
 /// <reference path="rx/rx.d.ts" />
 /// <reference path="rx/rx-lite.d.ts" />
